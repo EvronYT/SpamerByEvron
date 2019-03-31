5b501531a6db9287d792f9ce44081766272e8441
EvronYT <48995299+EvronYT@users.noreply.github.com>
2019-03-31 15:32:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,39 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client1 = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.client()
+const client4 = new Discord.client()
+const client5 = new Discord.client()
+const client6 = new Discord.client()
+const client7 = new Discord.client()
+const client8 = new Discord.client()
+const client9 = new Discord.client()
+const client10 = new Discord.client()
+const client11 = new Discord.client()
+const client12 = new Discord.client()
+const client13 = new Discord.client()
+const client14 = new Discord.client()
+const client15 = new Discord.client()
+const client16 = new Discord.client()
+const client17 = new Discord.client()
+const client18 = new Discord.client()
+const client19 = new Discord.client()
+const client20 = new Discord.client()
+const client21 = new Discord.client()
+const client22 = new Discord.client()
+const client23 = new Discord.client()
+const client24 = new Discord.client()
+const client25 = new Discord.client()
+const client26 = new Discord.client()
+const client27 = new Discord.client()
+const client28 = new Discord.client()
+const client29 = new Discord.client()
+const client30 = new Discord.client()
+
+
+
+
+
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -10,8 +43,13 @@ client.on('ready', () => {
    console.log(`Loadinng.`);
    console.log(`Loadinng..`);
    console.log(`Loadinng...`);
+   console.log(`Loadinng....`);
+   console.log(`Loadinng.....`);
+   console.log(`Loadinng......`);
+   console.log(`Loadinng.......`);
+   console.log(`Loadinng........`);
    console.log(`This Bots Online ' `);
-   console.log(`----------------`);
+   console.log(`بلش السبام ياعمري.`);
 });
 
 client.on('message',function(message) {
@@ -52,33 +90,33 @@ channel.send(`**Evron :beers:  The Best :smoking:  Youtuber :heartpulse:  & Disc
 
 
 
-client.login(process.env.TOKEN1);
-client.login(process.env.TOKEN2);
-client.login(process.env.TOKEN3);
-client.login(process.env.TOKEN4);
-client.login(process.env.TOKEN5);
-client.login(process.env.TOKEN6);
-client.login(process.env.TOKEN7);
-client.login(process.env.TOKEN8);
-client.login(process.env.TOKEN9);
-client.login(process.env.TOKEN10);
-client.login(process.env.TOKEN11);
-client.login(process.env.TOKEN12);
-client.login(process.env.TOKEN13);
-client.login(process.env.TOKEN14);
-client.login(process.env.TOKEN15);
-client.login(process.env.TOKEN16);
-client.login(process.env.TOKEN17);
-client.login(process.env.TOKEN18);
-client.login(process.env.TOKEN19);
-client.login(process.env.TOKEN20);
-client.login(process.env.TOKEN21);
-client.login(process.env.TOKEN22);
-client.login(process.env.TOKEN23);
-client.login(process.env.TOKEN24);
-client.login(process.env.TOKEN25);
-client.login(process.env.TOKEN26);
-client.login(process.env.TOKEN27);
-client.login(process.env.TOKEN28);
-client.login(process.env.TOKEN29);
-client.login(process.env.TOKEN30);
+client1.login(process.env.TOKEN1);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
+client6.login(process.env.TOKEN6);
+client7.login(process.env.TOKEN7);
+client8.login(process.env.TOKEN8);
+client9.login(process.env.TOKEN9);
+client10.login(process.env.TOKEN10);
+client11.login(process.env.TOKEN11);
+client12.login(process.env.TOKEN12);
+client13.login(process.env.TOKEN13);
+client14.login(process.env.TOKEN14);
+client15.login(process.env.TOKEN15);
+client16.login(process.env.TOKEN16);
+client17.login(process.env.TOKEN17);
+client18.login(process.env.TOKEN18);
+client19.login(process.env.TOKEN19);
+client20.login(process.env.TOKEN20);
+client21.login(process.env.TOKEN21);
+client22.login(process.env.TOKEN22);
+client23.login(process.env.TOKEN23);
+client24.login(process.env.TOKEN24);
+client25.login(process.env.TOKEN25);
+client26.login(process.env.TOKEN26);
+client27.login(process.env.TOKEN27);
+client28.login(process.env.TOKEN28);
+client29.login(process.env.TOKEN29);
+client30.login(process.env.TOKEN30);
